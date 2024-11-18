a1dfe820c3e3326c496923e6bb16b6364f0db174
Logickin-Lambda <logickinlambda@protonmail.com>
2024-11-18 14:21:09 +1100

    - Trying to figuring out how to do a 2D array list Although I now have a working solution, I am not sure if that cause memory leak. I will take a further investigation for this part, but now, let me move onto the hashmap.

--- src/arraylist_experiment.zig
+++ src/arraylist_experiment.zig
@@ -22,6 +22,7 @@ pub fn experimenting_array_list(bw: anytype, stdout: anytype) !void {
     // I wanna use the arenaAllocator for a list, let see how to make one:
     const page = std.heap.page_allocator;
     var arena = std.heap.ArenaAllocator.init(page);
+    defer arena.deinit();
 
     // Now, let me build another ArrayList again, but with arena allocator
     // Now I know why I got an error on using the arena allocator. It turns out
@@ -53,5 +54,34 @@ pub fn experimenting_array_list(bw: anytype, stdout: anytype) !void {
 
     try stdout.print("\n{s}\n", .{list2.items[0]});
     try stdout.print("{s}\n", .{list2.items[1]});
+    try bw.flush();
+
+    // I am going to build an identity matrix this time, just to see how I can do a nested array list
+    var matrix = std.ArrayList(std.ArrayList(i32)).init(arena.allocator());
+    defer matrix.deinit();
+
+    // I decided to do the dumb way just to understand how indicing in a nested array works
+    const array_size = 10;
+    for (0..array_size) |i| {
+        var column = std.ArrayList(i32).init(arena.allocator());
+
+        for (0..array_size) |j| {
+            if (i == j) {
+                try column.append(1);
+            } else {
+                try column.append(0);
+            }
+        }
+
+        try matrix.append(column);
+    }
+
+    for (0..array_size) |i| {
+        for (0..array_size) |j| {
+            try stdout.print("{d:<3}", .{matrix.items[i].items[j]});
+        }
+        try stdout.print("\n", .{});
+    }
+
     try bw.flush();
 }
