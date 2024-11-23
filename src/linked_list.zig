6837ac7d5ee6594438df590bbcd9ddfa75821358
Logickin-Lambda <logickinlambda@protonmail.com>
2024-11-23 19:17:34 +1100

    - Push function for Linked List

--- src/linked_list.zig
+++ src/linked_list.zig
@@ -2,20 +2,45 @@
 /// Let's do a link list to see if I can apply all the stuff learn from the previous test
 const std = @import("std");
 
+const LinkedListError = error{
+    IndexOutOfBound,
+    ElementNotFound,
+};
+
 pub fn LinkedList(comptime T: type) type {
     return struct {
         const Self = @This();
+        const Node = struct {
+            item: T,
+            node_ref: *?Node,
+        };
 
-        data: T,
-        node_ref: *LinkedList,
-        allocator: *std.mem.Allocator,
+        head: *?Node = null,
+        tail: *?Node = null,
+        size: usize,
+        allocator: std.mem.Allocator,
 
-        fn init(self: *Self, allocator: *std.mem.Allocator) Self {
-            return try allocator.create(self);
+        fn init(allocator: std.mem.Allocator) Self {
+            return try Self{ .allocator = allocator };
         }
-
         // deinit will be tough because it requires to recursively
         // remove all the linked elements
         // pub fn deinit(self: *Self) void {}
+
+        fn push(self: *Self, item: T) !void {
+            const node = try self.allocator.create(Node);
+            node.*.item = item;
+
+            // optional pattern with the reference to the object
+            if (self.tail) |*prev_node| {
+                prev_node.node_ref = node;
+                self.tail = node;
+            } else {
+                self.head = node;
+                self.tail = node;
+            }
+
+            self.size += 1;
+        }
     };
 }
